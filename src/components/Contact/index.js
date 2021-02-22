da9c6b092194bc145c074b562ebe222850ad12be
amiedawn <amie.d.carroll@gmail.com>
2021-02-22 11:49:04 -0700

    control state with controlled component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+// clear the input fields on the component loading
+
 
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+  function handleChange(e) {
+    setFormState({...formState, [e.target.name]: e.target.value })
+  }
+  console.log(formState);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState)
+  }
 
   return (
     <section>
-      <h1>Contact me</h1>
-      <form id="contact-form">
+      <h1>Contact Me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
         </div>
         <div>
           <label htmlFor="email">Email Address:</label>
-          <input type="email" name="email" />
+          <input type="email" defaultValue={email} name="email" onChange={handleChange}/>
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
         </div>
-        <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     </section>
   );
